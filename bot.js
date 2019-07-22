26ab01bcf1af7e98855830823ab9e396f0428fa2
Lildawg <38541049+Lildawg@users.noreply.github.com>
2019-07-22 03:27:04 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,37 +1,26 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
+
+ 
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+
+    console.log('I am ready!');
+
 });
-client.on('message', msg => {
-  if (msg.content === '+ping') {
-    msg.reply('Pong!');
-  }
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === '+hi') {
+
+       message.reply('Hello.');
+
+       }
+
 });
-client.on('message', msg => {
-  if (msg.content === '+hi') {
-    msg.reply('Hello.');
-  }
-});
-client.on('message', msg => {
-  if (msg.content === '+cake') {
-    msg.reply('Mmmmm... Cake...');
-  }
-});
-client.on('message', msg => {
-  if (msg.content === '+kebab') {
-    msg.reply('We must remove kebab!');
-  }
-});
-client.on('message', msg => {
-  if (msg.content === '+bob') {
-    msg.reply('Bob the Builder! CAN HE FIX IT?! Bob the Builder! No... No he fucking can`t');
-  }
-});
-client.on('message', msg => {
-  if (msg.content === '+kiss') {
-    msg.reply('**Slap!** I am already married.');
-  }
-});
+
 
 client.login(process.env.NDI1ODE3NjQ3Mjk3NTkzMzQ0.XTVuTQ.owouDQrwQo92xnNqXIrVWFXE0mg)
